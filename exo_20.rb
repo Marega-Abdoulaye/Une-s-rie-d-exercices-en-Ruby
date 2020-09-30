a035f6b03d6f664106eea409144f2793477bd146
Marega-Abdoulaye <71715054+Marega-Abdoulaye@users.noreply.github.com>
2020-09-30 08:54:09 +0200

    Add files via upload

---
+++ exo_20.rb
@@ -0,0 +1,20 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+
+height = gets.chomp.to_i
+
+i = "#"
+
+
+if height >25 or height <1
+
+	puts "La pyramide contient entre 1 et 25 étages."
+
+ else  
+   puts "Voici la pyramide :"
+
+	height.times do 
+    puts i
+    i = i + "#"
+	end
+
+end
